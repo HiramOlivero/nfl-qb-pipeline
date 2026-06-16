b2b0049191b20d280cf40011f75acb1e9c11fc45
Hiram <olivero.hiram@gmail.com>
2026-06-16 16:01:25 -0600

    update models & project structure

--- src/models.py
+++ src/models.py
@@ -1,7 +1,14 @@
+from __future__ import annotations
+
 import os
-from sqlalchemy import create_engine, Column, Integer, String, Float, Date, Text
-from sqlalchemy.orm import declarative_base
+
 from dotenv import load_dotenv
+from sqlalchemy import Column, Date, DateTime, Float, Integer, String, Text, create_engine
+from sqlalchemy.orm import declarative_base
+
+# This file documents the Bronze layer schema.
+# Tables are created automatically by the Bronze pipeline.
+# Run this script only to reset the schema in a new environment.
 
 load_dotenv()
 
@@ -11,11 +18,12 @@ Base = declarative_base()
 
 class RawMayePlays(Base):
     __tablename__ = 'raw_maye_plays'
-    # id = Column(Integer, primary_key=True)
+
+    # id's
     play_id = Column(Integer, primary_key=True)
     game_id = Column(String, primary_key=True)
 
-    # Contexto
+    # Game context
     season = Column(Integer)
     season_type = Column(String(20))
     game_date = Column(Date)
@@ -24,7 +32,7 @@ class RawMayePlays(Base):
     away_team = Column(String)
     defteam = Column(String)
 
-    # Situacion del juego
+    # Game situation
     drive = Column(Integer)
     qtr = Column(Integer)
     down = Column(Integer)
@@ -32,7 +40,7 @@ class RawMayePlays(Base):
     desc = Column(Text)
     play_type = Column(String)
 
-    # Meticas
+    # Advanced metrics
     yards_gained = Column(Float)
     epa = Column(Float)
     qb_epa = Column(Float)
@@ -41,24 +49,32 @@ class RawMayePlays(Base):
     xyac_epa = Column(Float)
     cpoe = Column(Float)
 
-    # Estadisticas de pase
+    # Passing stats
     pass_attempt = Column(Integer)
     complete_pass = Column(Integer)
     passing_yards = Column(Float)
     air_yards = Column(Float)
+    yards_after_catch = Column(Float)
     pass_touchdown = Column(Integer)
     interception = Column(Integer)
     sack = Column(Integer)
     pass_location = Column(String)
+    pass_lenght = Column(String)
     td_team = Column(String)
     td_player_name = Column(String)
+    receiver_player_name = Column(String)
 
-    # Estadisticas de carrera
+    # QB metrics
+    qb_hit = Column(Integer)
+    qb_scramble = Column(Integer)
+    qb_dropback = Column(Integer)
+
+    # Rushing stats
     rush_attempt = Column(Integer)
     rushing_yards = Column(Float)
     rush_touchdown = Column(Integer)
 
-    # Eventos
+    # Fubmles & conversions
     fumble = Column(Integer)
     fumble_lost = Column(Integer)
     fumble_forced = Column(Integer)
@@ -66,8 +82,15 @@ class RawMayePlays(Base):
     fumble_out_of_bounds = Column(Integer)
     two_point_conv_result = Column(String)
 
+    # Scoreboard
+    score_differential = Column(Integer)
+    posteam_score =  Column(Integer)
+    defteam_score = Column(Integer)
+
+    # Audit
+    bronze_loaded_at = Column(DateTime(timezone=True))
 
 if __name__ == '__main__':
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
-    print('Esquema actualizado, la tabla ya tiene todo sobre drake maye. ')
+    print('Schema updated succesfully ')
